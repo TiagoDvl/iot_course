3265fe96a05ac0cf6610be5f39532785ee5735d5
TiagoDvl <tdavilas@gmail.com>
2016-10-25 20:47:24 -0200

    Timer feito mais bonitinho só falta o retry

--- TaskForGrade/example/prog.c
+++ TaskForGrade/example/prog.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <avr/io.h>
@@ -8,14 +9,20 @@ volatile char* pb = (char*) 0x25;
 volatile char* ddr = (char*) 0x24;
 volatile char* pin = (char*) 0x23;
 
-int MAX_INTERRUPT_TIMES = 32;   // This was a random number that i found out to be kind of 1 second.
 bool isGameStarted;
 bool isLedUp;
 
 void setupPreScaler() {
-    TCCR0B = (1<<CS02) | (1<<CS00); // Set timer0 with /1024 prescaler
-    //     TCCR0B = (1<<CS02); // Set timer0 with /256 prescaler
-    //     TCCR0B = (1<<CS01) | (1<<CS00); // Set timer0 with /64 prescaler
+    int r = rand() % 4;
+    printf("Random Number: %d", r);
+    printf("\n");
+    
+    
+    TCCR1B |= (1 << WGM12) | (1<<CS02) | (1<<CS00); // Set timer0 with /1024 prescaler
+    //     TCCR1B |= (1 << WGM12) | (1<<CS02); // Set timer0 with /256 prescaler
+    //     TCCR1B |= (1 << WGM12) | (1<<CS00); // Set timer0 with /64 prescaler
+    
+    OCR1A = 16 * 1000 * r; // Since prescaller set poke to be called each 1ms, multiply by 1000 gives 1 second.
 }
 
 void turnLED(bool ledState) {
@@ -26,12 +33,20 @@ void turnLED(bool ledState) {
     }
 }
 
-bool isLEDOn() {
-    return *pin & (1<<0);
+void turnTimeInterrupt(bool interruptState) {
+    if (interruptState) {
+        TIMSK1 |= (1 << OCIE1A);
+    } else {
+        TIMSK1 &= ~(1 << OCIE1A);
+    }
+    
 }
 
-int main(void)
-{
+bool isLEDOn() {
+    return *pin & (1<<5);
+}
+
+int main(void) {
     uart_init();
     stdout = &uart_output;
     stdin  = &uart_input;
@@ -41,14 +56,14 @@ int main(void)
     
     *ddr |= (1<<5); // Preparando ele pra output
     DDRB &= ~(1 << DDB0);         // Clear the PB0 pin
-                                // PB0 (PCINT0 pin) is now an input
+    // PB0 (PCINT0 pin) is now an input
     PORTB |= (1 << PORTB0);        // turn On the Pull-up
-                                // PB0 is now an input with pull-up enabled
+    // PB0 is now an input with pull-up enabled
     
     PCICR |= (1 << PCIE0);     // set PCIE0 to enable PCMSK0 scan
     PCMSK0 |= (1 << PCINT0);   // set PCINT0 to trigger an interrupt on state change 
     
-    TIMSK0=(1<<TOIE0); // Enable the interrupt on overflow for Timer0
+    turnTimeInterrupt(true);
     
     
     *pb &= ~(1<<5); // Desligando o LED?
@@ -56,39 +71,36 @@ int main(void)
     
     sei();                     // turn on interrupts
     
-    while(1)
-    {
+    while (1) {
         
     }
 }
 
-ISR (PCINT0_vect)
-{
-    if( (PINB & (1 << PINB0)) == 1 )
-    {
+ISR (PCINT0_vect) {
+    if( (PINB & (1 << PINB0)) == 1 ) {
         if (!isGameStarted) {
-        
             isGameStarted = true;
             printf("Game Started! Good Luck. ;)");
+            printf("\n");
             setupPreScaler();
             return;
         }
         
-        if (isGameStarted && isLEDOn()) {
-            turnLED(false);
-            printf("Time Spent: %d", (int) TCNT0);
-            // Mede o Tempo
-        }
+        if (isGameStarted) {
+            if (isLEDOn() && ((int) TCNT1 > 0)) {
+                turnLED(false);
+                printf("Pressed in: %d time", (int) TCNT1);
+                printf("\n");
+            } else {
+                turnTimeInterrupt(true);
+            }
+        } 
     }
 }
 
-ISR(TIMER0_OVF_vect)
-{
-    if (MAX_INTERRUPT_TIMES == 0) {
-        turnLED(true);
-        TCNT0=0x00; // Set timer0 counter initial value to 0
-    } else {
-        MAX_INTERRUPT_TIMES --;
-    }
+ISR(TIMER1_COMPA_vect) {   
+    turnLED(true);
+    TCNT1=0x00; 
+    turnTimeInterrupt(false);
 }
 
